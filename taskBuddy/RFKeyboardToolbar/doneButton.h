fb115a7d40bb3c4d5483bf6b0a9e32ec6d2e58b1
iOS Apps Development <info@iosappsdev.org>
2014-01-19 15:27:49 -0800

    Code Enhacements

---
+++ taskBuddy/RFKeyboardToolbar/doneButton.h
@@ -0,0 +1,13 @@
+//
+//  doneButton.h
+//  taskbuddy
+//
+//  Created by Ricardo Ruiz on 1/16/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import "RFToolbarButton.h"
+
+@interface doneButton : RFToolbarButton
+
+@end
